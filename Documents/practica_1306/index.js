1efea619a858e80e8a3990a6170f896ce6d55926
Miguel Angel Jimenez <miguel_ajs@yahoo.com>
2020-06-09 22:18:07 -0400

    Initial Commit

---
+++ Documents/practica_1306/index.js
@@ -0,0 +1,14 @@
+/*
+Practica del dia 13 de junio de 2020
+ Realizado por Ing. Miguel Angel Jiménez
+ */
+
+ // Definicion de variables
+let nombre = "Miguel Angel";
+let apellido_p = "Jiménez";
+let apellido_m = "Solano";
+
+// Despliegue de alertas
+   alert("Mi nombre es: "+nombre);
+   alert("Mi apellido paterno es: "+apellido_p);
+   alert("Mi apellido materno es: "+apellido_m);
